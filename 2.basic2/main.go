cb3738f925539e80892404c55a69ec203c8448c3
yamini <yaminipureddiwar@gmail.com>
2026-08-22 00:14:06 +0530

    updated

--- 2.basic2/main.go
+++ 2.basic2/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func printMessage(id int) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Goroutine", id, ":", i)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
 
 func main() {
-	fmt.Println("welcome")
+	go printMessage(1)
+	go printMessage(2)
+	go printMessage(3)
+	go printMessage(4)
+	go printMessage(5)
+
+	time.Sleep(3 * time.Second)
 }
